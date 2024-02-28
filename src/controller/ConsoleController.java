34221b003d462c8febc47443b1161db6bada4a05
NeonVanadium <jackwhitten@outlook.com>
2024-02-28 00:03:12 -0500

    Allow string-based matching in console controller

--- src/controller/ConsoleController.java
+++ src/controller/ConsoleController.java
@@ -26,17 +26,38 @@ public class ConsoleController implements Controller {
   public int pickOption() {
     int selected = -1;
     while (!isValidSelection(selected)) {
-      selected = input.nextInt();
+      String resp = input.nextLine();
+      selected = parseInt(resp);
+      if (selected == -1) { // non-integer response. Try string matching.
+        for (int i = 0; i < options.size(); i++) {
+          if (options.get(i).toLowerCase().startsWith(resp.toLowerCase())) {
+            selected = i;
+          }
+        }
+      }
       if (!isValidSelection(selected)) {
         view.print("Not a valid input.");
       }
     }
-    view.print("Picked " + options.get(selected - 1));
-    return selected - 1;
+    view.print("Picked " + options.get(selected));
+    return selected;
   }
 
   private boolean isValidSelection(int s) {
-    return s > 0 && s < options.size() + 1;
+    return s >= 0 && s < options.size();
+  }
+
+  /**
+   * Parse int from a response string. Doesn't support negatives. If parsing fails,
+   * returns -1.
+   */
+  private int parseInt(String s) {
+    if (Character.isDigit(s.charAt(0))) {
+      try {
+        return Integer.parseInt(s) - 1;
+      } catch (Exception e) {}
+    }
+    return -1;
   }
 
 }
