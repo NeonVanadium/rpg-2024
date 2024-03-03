fedd74021ccc6bfd71dd9550c795799b30d093b5
NeonVanadium <jackwhitten@outlook.com>
2024-03-03 00:40:46 -0500

    Create CharacterManager.java

---
+++ src/game/characters/CharacterManager.java
@@ -0,0 +1,27 @@
+package game.characters;
+
+import game.Util;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CharacterManager {
+  private static Map<String, GameCharacter> characters = new HashMap();
+
+  public static void loadCharacters() {
+    Util.parseFileAndDoEachLine("resources\\characters.txt",
+        CharacterManager::makeCharacterFromLine);
+  }
+
+  private static void makeCharacterFromLine(String line) {
+    String[] lineParts = line.split(" "); // note that 0th will be Util.ENTRY_START_SYMBOL
+    String label = lineParts[1].trim();
+    Gender gender = Gender.getFromString(lineParts[2].trim());
+    System.out.println("Character label " + label);
+    characters.put(label, new GameCharacter(label, gender));
+  }
+
+  public static GameCharacter getCharacterByLabel(String label) {
+    return characters.get(label);
+  }
+
+}
